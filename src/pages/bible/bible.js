35ae7ff362e53e4fce1b0d867aaf9dc8c2be504b
Sarah Marinho <sarah.mar.figueiredo@gmail.com>
2024-06-21 18:14:48 -0300

    Todos os livros (agr com o arquivo salvo pq tinha esquecido rsrsrs)

--- src/pages/bible/bible.js
+++ src/pages/bible/bible.js
@@ -31,76 +31,55 @@ const books = [
     createData(17, "Ester", 10, 959),
     createData(18, "Jó", 42, 959),
     createData(19, "Salmos", 150, 959),
-    createData(20, "Salmos", 150, 959),
-    createData(21, "Salmos", 150, 959),
-    createData(22, "Salmos", 150, 959),
-    createData(23, "Salmos", 150, 959),
-    createData(24, "Salmos", 150, 959),
-    createData(25, "Salmos", 150, 959),
-    createData(26, "Salmos", 150, 959),
-    createData(27, "Salmos", 150, 959),
-    createData(28, "Salmos", 150, 959),
-    createData(29, "Salmos", 150, 959),
-    createData(30, "Salmos", 150, 959),
-    createData(31, "Salmos", 150, 959),
-    createData(32, "Salmos", 150, 959),
-    createData(33, "Salmos", 150, 959),
-    createData(34, "Salmos", 150, 959),
-    createData(35, "Salmos", 150, 959),
-    createData(36, "Salmos", 150, 959),
-    createData(37, "Salmos", 150, 959),
-    createData(38, "Salmos", 150, 959),
-
+    createData(20, "Provérbios", 31, 959),
+    createData(21, "Eclesiastes", 12, 959),
+    createData(22, "Cantares", 8, 959),
+    createData(23, "Isaías", 66, 959),
+    createData(24, "Jeremias", 52, 959),
+    createData(25, "Lamentações", 5, 959),
+    createData(26, "Ezequiel", 48, 959),
+    createData(27, "Daniel", 12, 959),
+    createData(28, "Oséias", 14, 959),
+    createData(29, "Joel", 3, 959),
+    createData(30, "Amós", 9, 959),
+    createData(31, "Obadias", 1, 959),
+    createData(32, "Jonas", 4, 959),
+    createData(33, "Miqueias", 7, 959),
+    createData(34, "Naum", 3, 959),
+    createData(35, "Habacuque", 3, 959),
+    createData(36, "Sofonias", 3, 959),
+    createData(37, "Ageu", 2, 959),
+    createData(38, "Zacarias", 14, 959),
+    createData(39, "Malaquias", 4, 959),
+    createData(40, "Mateus", 28, 959),
+    createData(41, "Marcos", 16, 959),
+    createData(42, "Lucas", 24, 959),
+    createData(43, "João", 21, 959),
+    createData(44, "Atos dos Apóstolos", 28, 959),
+    createData(45, "Romanos", 16, 959),
+    createData(46, "1 Coríntios", 16, 959),
+    createData(47, "2 Coríntios", 13, 959),
+    createData(48, "Gálatas", 6, 959),
+    createData(49, "Efésios", 6, 959),
+    createData(50, "Filipenses", 4, 959),
+    createData(51, "Colossenses", 4, 959),
+    createData(52, "1 Tessalonicenses", 5, 959),
+    createData(53, "2 Tessalonicenses", 3, 959),
+    createData(54, "1 Timóteo", 6, 959),
+    createData(55, "2 Timóteo", 4, 959),
+    createData(56, "Tito", 3, 959),
+    createData(57, "Filemom", 1, 959),
+    createData(58, "Hebreus", 13, 959),
+    createData(59, "Tiago", 5, 959),
+    createData(60, "1 Pedro", 5, 959),
+    createData(61, "2 Pedro", 3, 959),
+    createData(62, "1 João", 5, 959),
+    createData(63, "2 João", 1, 959),
+    createData(64, "3 João", 1, 959),
+    createData(65, "Judas", 1, 959),
+    createData(19, "Apocalipse", 22, 959),
 ];
 
-// Salmos	150
-// Provérbios	31
-// Eclesiastes	12
-// Cânticos	8
-// Isaías	66
-// Jeremias	52
-// Lamentações	5
-// Ezequiel	48
-// Daniel	12
-// Oséias	14
-// Joel	3
-// Amós	9
-// Obadias	1
-// Jonas	4
-// Miquéias	7
-// Naum	3
-// Habacuque	3
-// Sofonias	3
-// Ageu	2
-// Zacarias	14
-// Malaquias	4
-// Mateus	28
-// Marcos	16
-// Lucas	24
-// João	21
-// Atos dos Apóstolos	28
-// Romanos	16
-// 1 Coríntios	16
-// 2 Coríntios	13
-// Gálatas	6
-// Efésios	6
-// Filipenses	4
-// Colossenses	4
-// 1 Tessalonicenses	5
-// 2 Tessalonicenses	3
-// 1 Timóteo	6
-// 2 Timóteo	4
-// Tito	3
-// Filemom	1
-// Hebreus	13
-// Tiago	5
-// 1 Pedro	5
-// 2 Pedro	3
-// 1 João	5
-// 2 João	1
-// 3 João	1
-// Judas	1
-// Apocalipse	22
 
 export default function Bible() {
     return (
